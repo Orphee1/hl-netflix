86803f4f6ffca361cc809ef39441788bf0ec114a
Orphee1 <hugolattard@gmail.com>
2021-11-17 22:19:05 +0100

    Added live search

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,6 @@
+/* eslint-disable */
 import React, { useContext, useState, useEffect } from 'react'
+import Fuse from 'fuse.js'
 import { SelectProfileContainer } from './profile.js'
 import { FooterContainer } from './footer'
 import { FirebaseContext } from '../context/firebase'
@@ -25,6 +27,18 @@ export function BrowseContainer({ slides }) {
   useEffect(() => {
     setSlideRows(slides[category])
   }, [slides, category])
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ['data.description', 'data.title', 'data.genre'],
+    })
+    const results = fuse.search(searchTerm).map(({ item }) => item)
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results)
+    } else {
+      setSlideRows(slides[category])
+    }
+  }, [searchTerm])
 
   return profile.displayName ? (
     <>
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,8 +1,7 @@
-/* eslint-disable */
 import Firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
-import { seedDatabase } from '../seed'
+// import { seedDatabase } from '../seed'
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
